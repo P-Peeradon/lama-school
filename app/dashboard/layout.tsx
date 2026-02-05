b0194703dff85216f5ef7a4cb3e381eff0c019d8
P-Peeradon <p.determinant.integrate3@gmail.com>
2026-02-06 08:49:32 +1100

    Apply styling on menu.

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
+import Menu from "../components/Menu";
+import Navbar from "../components/Navbar";
 
 export default function DashboardLayout({
     children,
@@ -14,9 +16,13 @@ export default function DashboardLayout({
                     <Image src="/logo.png" alt="logo" width={32} height={32} />
                     <span className="hidden lg:block">SchooLama</span>
                 </Link>
+                <Menu />
             </div>
             {/* RIGHT */}
-            <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-200"></div>
+            <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-200 overflow-scroll">
+                <Navbar />
+                {children}
+            </div>
         </div>
     );
 };
