3d4fa2a37b9acf14023363dabaa62d5d3a05ef6c
P-Peeradon <p.determinant.integrate3@gmail.com>
2026-02-06 09:26:33 +1100

    Change background color

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -11,7 +11,7 @@ export default function DashboardLayout({
     return (
         <div className="h-screen flex">
             {/* LEFT */}
-            <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200">
+            <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-zinc-200">
                 <Link href="/" className="flex items-center justify-center gap">
                     <Image src="/logo.png" alt="logo" width={32} height={32} />
                     <span className="hidden lg:block">SchooLama</span>
@@ -19,7 +19,7 @@ export default function DashboardLayout({
                 <Menu />
             </div>
             {/* RIGHT */}
-            <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-200 overflow-scroll">
+            <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-white  overflow-y-scroll">
                 <Navbar />
                 {children}
             </div>
