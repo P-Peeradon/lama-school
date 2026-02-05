3b8a0df2ff997e3e27d5e6e7cfe5765d77f9c1d3
P-Peeradon <p.determinant.integrate3@gmail.com>
2026-02-06 09:26:26 +1100

    Create navbar

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,30 @@
+import Image from 'next/image';
+
+const Navbar = () => {
+    return (
+        <nav className='flex items-center justify-between p-4'>
+            {/* SEARCH BAR */}
+            <div className="hidden md:flex">
+                <Image src="/search.png" alt="" width={14} height={14} />
+                <input type="text" placeholder="search..." />
+            </div>
+            {/* ICONS AND USER */}
+            <div className="flex items-center gap-6 text-xs rounded-full ring-[1.5px] ring-gray-300 px-2">
+                <div className="bg-white rounded-full w-7 flex items-center justify-center cursor-pointer">
+                    <Image src="/message.png" alt="" width={20} height={20} />
+                </div>
+                <div className="bg-white rounded-full w-7 flex items-center justify-center cursor-pointer relative">
+                    <Image src="/announcement.png" alt="" width={20} height={20} />
+                    <div className="absolute -top-3 right-3 w-5 h-5 flex items-center justify-center bg-purple-500 rounded-full text-xs">1</div>
+                </div>
+                <div className="flex flex-col">
+                    <span className="text-xs leading-3 font-medium text-black">John Doe</span>
+                    <span className="text-[10px] text-black">Admin</span>
+                </div>
+                <Image src='/avatar.png' alt='' width={36} height={36} />
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
