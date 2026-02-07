401118fe2f5a3eadcd1c2462ff9830ea09b1c28c
P-Peeradon <p.determinant.integrate3@gmail.com>
2026-02-08 09:26:41 +1100

    Add image and bottom data for chart.

--- app/components/CountChart.tsx
+++ app/components/CountChart.tsx
@@ -4,9 +4,14 @@ import Image from "next/image"
 
 const data = [
     {
+        name: "Total",
+        count: 106,
+        fill: "white"
+    },
+    {
         name: "Girls",
         count: 53,
-        fill: "#8884d8"
+        fill: "#FAE27C"
     },
     {
         name: "Boys",
@@ -20,19 +25,24 @@ const CountChart = () => {
         <div className="bg-white rounded-xl w-full h-full p-4">
             <div className="">
                 <h1>Students</h1>
-                <Image src="/moreDark.png" alt='' />
+                <Image src="/moreDark.png" alt='' width={20} height={20} />
             </div>
-            <div className="">
-                <ResponsiveContainer width="100%" height="100%">
-                    <RadialBarChart cx="50%" cy="50%" innerRadius="10%" outerRadius="80%" barSize={10} data={data}>
+            <div className="relative w-full h-[75%]">
+                <ResponsiveContainer>
+                    <RadialBarChart cx="50%" cy="50%" innerRadius="40%" outerRadius="100%" barSize={15} data={data}>
                         <RadialBar
-                            label={{ position: 'insideStart', fill: '#fff'}}
                             background
-                            dataKey="name"
+                            dataKey="count"
                         />
-                        <Legend iconSize={10} layout="vertical" verticalAlign="middle" />
                     </RadialBarChart>
                 </ResponsiveContainer>
+                <Image 
+                    src="/maleFemale.png" 
+                    alt="" 
+                    width={50} 
+                    height={50} 
+                    className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" 
+                />
             </div>
             {/* BOTTOM */}
             <div className="flex justify-center gap-16">
@@ -40,6 +50,12 @@ const CountChart = () => {
                     <div className="w-5 h-5 bg-[#C3EBFA] rounded-full">
                         <h1 className="font-bold">1234</h1>
                         <h2 className="text-xs text-gray-500">Boys (55%)</h2>
+                    </div>
+                </div>
+                <div className="flex flex-col gap-1">
+                    <div className="w-5 h-5 bg-[#FAE17C] rounded-full">
+                        <h1 className="font-bold">1234</h1>
+                        <h2 className="text-xs text-gray-500">Girls (55%)</h2>
                     </div>
                 </div>
             </div>
