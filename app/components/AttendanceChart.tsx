b4d109fcc66987f5d1df55a4f8faf8bdf761039d
P-Peeradon <p.determinant.integrate3@gmail.com>
2026-02-08 09:53:50 +1100

    Complete styling

--- app/components/AttendanceChart.tsx
+++ app/components/AttendanceChart.tsx
@@ -31,20 +31,18 @@ const data = [
 ];
 
 const AttendanceChart = () => {
-    const isAnimationActive = true;
-
     return (
         <div className="bg-white rounded-lg p-4 h-full">
-            <div className="">
-                <h1 className="font-bold">Attendance</h1>
+            <div className="flex justify-between items-center">
+                <h1 className="font-semibold text-lg">Attendance</h1>
                 <Image src="/moreDark.png" alt="" width={20} height={20} />
             </div>
             <ResponsiveContainer width="100%" height="90%" >
                 <BarChart width={500} height={300} data={data} barSize={20}>
                     <CartesianGrid strokeDasharray="3 3" vertical={false} stroke="#ddd" />
-                    <XAxis dataKey="name" axisLine={false} />
-                    <YAxis axisLine={false} />
-                    <Tooltip />
+                    <XAxis dataKey="name" axisLine={false} tick={{ fill: "#d1d5db "}} tickLine={false} />
+                    <YAxis axisLine={false} tick={{ fill: "#d1d5db "}} tickLine={false} />
+                    <Tooltip contentStyle={{ borderRadius: "10px", borderColor: "lightgray"}} />
                     <Legend 
                         align="left" 
                         verticalAlign="top" 
--- app/components/CountChart.tsx
+++ app/components/CountChart.tsx
@@ -23,13 +23,20 @@ const data = [
 const CountChart = () => {
     return (
         <div className="bg-white rounded-xl w-full h-full p-4">
-            <div className="">
-                <h1 className="font-bold">Students</h1>
+            <div className="flex justify-between items-center">
+                <h1 className="font-semibold text-lg">Students</h1>
                 <Image src="/moreDark.png" alt='' width={20} height={20} />
             </div>
             <div className="relative w-full h-[75%]">
                 <ResponsiveContainer>
-                    <RadialBarChart cx="50%" cy="50%" innerRadius="40%" outerRadius="100%" barSize={15} data={data}>
+                    <RadialBarChart 
+                        cx="50%" 
+                        cy="50%" 
+                        innerRadius="40%" 
+                        outerRadius="100%" 
+                        barSize={15} 
+                        data={data}
+                    >
                         <RadialBar
                             background
                             dataKey="count"
