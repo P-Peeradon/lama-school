98666731d0d9cbf3e31c02bbaed0c9d87d1ffd6a
P-Peeradon <p.determinant.integrate3@gmail.com>
2026-02-08 09:17:16 +1100

    Create admin page with user card and charts position.

--- app/dashboard/admin/page.tsx
+++ app/dashboard/admin/page.tsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import UserCard from '@/app/components/UserCard';
+import CountChart from '@/app/components/CountChart';
 
 const AdminPage = () => {
     return (
-        <div>
-            Admin Page
+        <div className='p-4 flex gap-4 flex-col md:flex-row'>
+            {/* LEFT */}
+            <div className="w-full lg:w-2/3 flex flex-col gap-8">
+                {/* USER CARDS */}
+                <div className="flex gap-4 justify-between">
+                    <UserCard type="student" />
+                    <UserCard type="teacher" />
+                    <UserCard type="parent" />
+                    <UserCard type="staff" />
+                </div>
+                {/* MIDDLE CHART */}
+                <div className="flex gap-4 fex-col lg:flex-row">
+                    {/* COUNT CHART */}
+                    <div className="w-fll lg:w-1/3 h-112.5">
+                        <CountChart />
+                    </div>
+                    {/* ATTENDANCE CHART */}
+                </div>
+                {/* BOTTOM CHART */}
+            </div>
+            <div className="w-full lg:w-1/3"></div>
         </div>
     )
 };
