e34c588be355d0c094c97aef7dd77b049b3bac25
P-Peeradon <p.determinant.integrate3@gmail.com>
2026-02-09 21:56:20 +1100

    Finish layout styling

--- app/components/Menu.tsx
+++ app/components/Menu.tsx
@@ -122,12 +122,17 @@ const Menu = () => {
             {menuItems.map(i => (
                 <div key={i.title} className="flex flex-col gap-2">
                     <span className="hidden lg:block text-gray-400 font-light my-4">{i.title}</span>
-                    {i.items.map(item => (
-                        <Link href={item.href} key={item.label} className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2">
+                    {i.items.map((item) => {
+                      return (
+                        <Link 
+                          href={item.href} 
+                          key={item.label} 
+                          className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 md:px-2 rounded-md hover:bg-[#EDF9FD]"
+                        >
                             <Image src={item.icon} alt="" width={20} height={20} /> 
                             <span className="text-black hidden lg:block">{item.label}</span>
                         </Link>
-                    ))}
+                    )})}
                 </div>
             ))}
         </div>
