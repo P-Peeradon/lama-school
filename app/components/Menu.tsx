123db1e582caddce2a4f1acb633b8cc84d83a00c
P-Peeradon <p.determinant.integrate3@gmail.com>
2026-02-09 21:59:27 +1100

    Implement role base menu rendering.

--- app/components/Menu.tsx
+++ app/components/Menu.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import Image from "next/image";
 
+export const role = 'admin';
+
 const menuItems = [
     {
         title: "MENU",
@@ -123,16 +125,17 @@ const Menu = () => {
                 <div key={i.title} className="flex flex-col gap-2">
                     <span className="hidden lg:block text-gray-400 font-light my-4">{i.title}</span>
                     {i.items.map((item) => {
-                      return (
-                        <Link 
-                          href={item.href} 
-                          key={item.label} 
-                          className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 md:px-2 rounded-md hover:bg-[#EDF9FD]"
-                        >
-                            <Image src={item.icon} alt="" width={20} height={20} /> 
-                            <span className="text-black hidden lg:block">{item.label}</span>
-                        </Link>
-                    )})}
+                        if (item.visible.includes(role)) return (
+                            <Link 
+                                href={item.href} 
+                                key={item.label} 
+                                className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 md:px-2 rounded-md hover:bg-[#EDF9FD]"
+                            >
+                                <Image src={item.icon} alt="" width={20} height={20} /> 
+                                <span className="text-black hidden lg:block">{item.label}</span>
+                            </Link>
+                        )}
+                    )}
                 </div>
             ))}
         </div>
