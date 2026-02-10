d9d970a6cbd503bbe3a58d493b626456a94aab8b
P-Peeradon <p.determinant.integrate3@gmail.com>
2026-02-10 16:39:35 +1100

    Add scheduled events.

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -10,31 +10,31 @@ export const calendarEvents = [
     {
         title: 'Math',
         allDay: false,
-        start: new Date(2024, 7, 12, 8, 0),
-        end: new Date(2024, 7, 12, 8, 45)
+        start: new Date(2026, 2, 12, 8, 0),
+        end: new Date(2026, 2, 12, 8, 45)
     },
     {
         title: 'English',
         allDay: false,
-        start: new Date(2024, 7, 12, 9, 0),
-        end: new Date(2024, 7, 12, 9, 45)
+        start: new Date(2026, 2, 12, 9, 0),
+        end: new Date(2026, 2, 12, 9, 45)
     },
     {
         title: 'Biology',
         allDay: false,
-        start: new Date(2024, 7, 12, 10, 0),
-        end: new Date(2024, 7, 12, 10, 45)
+        start: new Date(2026, 2, 12, 10, 0),
+        end: new Date(2026, 2, 12, 10, 45)
     },
     {
         title: 'Physics',
         allDay: false,
-        start: new Date(2024, 7, 12, 11, 0),
-        end: new Date(2024, 7, 12, 11, 45)
+        start: new Date(2026, 2, 12, 11, 0),
+        end: new Date(2026, 2, 12, 11, 45)
     },
     {
         title: 'Chemistry',
         allDay: false,
-        start: new Date(2024, 7, 12, 13, 0),
-        end: new Date(2024, 7, 12, 13, 45)
+        start: new Date(2026, 2, 12, 13, 0),
+        end: new Date(2026, 2, 12, 13, 45)
     },
 ];
